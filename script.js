076afffe8e4ea9f4ade000555432763a2d8cb46d
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-07-14 20:56:47 +0530

    Remove unused navigation logic from script.js and clean up index.html

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Navigation Logic
-const list = document.querySelectorAll('.list');
-function activeLink() {
-    list.forEach((item) => item.classList.remove('active'));
-    this.classList.add('active');
-}
-list.forEach((item) => item.addEventListener('click', activeLink));
 
 // --- GLOBAL VARIABLE ---
 var finalGeneratedLink = "";
